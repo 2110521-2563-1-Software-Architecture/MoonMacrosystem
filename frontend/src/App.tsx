966bc680afd0314feb72bd0aec5991a46306fe07
icekang <namkang_chutisilp@hotmail.com>
2020-10-04 16:45:14 +0700

    🚀 First Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { DatePicker } from 'antd'
+import 'antd/dist/antd.css'
+
+export default function App() {
+  return (
+    <div>
+      Hello
+      <DatePicker />
+    </div>
+  )
+}
---
+++ frontend/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "none",
+  entry: {
+    app: path.join(__dirname, "src", "index.tsx"),
+  },
+  target: "web",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  devServer: {
+    stats: {
+      children: false,
+      maxModules: 0,
+    },
+    port: 3001,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ["ts-loader"],
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+};
